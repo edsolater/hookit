2203694fc373e2263fd9ecbf35d6a80c85d6334a
edsolater <edsolater@qq.com>
2022-06-10 00:52:37 +0800

    🎉 init

---
+++ src/hooks/useResizeObserver.tsx
@@ -0,0 +1,24 @@
+import { RefObject, useEffect } from 'react'
+
+import { Nullish } from '@edsolater/fnkit'
+
+/** for build-in ResizeObserverEntry need narrows down target manually which is tedious */
+interface GenericResizeObserverEntry<El extends Element> extends ResizeObserverEntry {
+  readonly target: El
+}
+
+export default function useResizeObserver<El extends Element>(
+  ref: RefObject<El | Nullish>,
+  callback: (entry: GenericResizeObserverEntry<El>, observer: ResizeObserver) => void
+) {
+  useEffect(() => {
+    if (!ref.current) return
+    const observer = new ResizeObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        callback(entry as any, observer)
+      })
+    })
+    observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [])
+}
